a3f65475d01d4596f15232cf33ced84da3986dc9
Olivier <olivier.winkler@icloud.com>
2023-12-25 14:55:12 +0100

    feat(api): add some more basic routes

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,18 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
 
+app.get('/api', (req, res) => {
+  const path = `/api/item/${crypto.randomUUID()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
